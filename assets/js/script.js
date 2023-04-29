606aa9b4eaaf606f4eea55a287d713f8e92efe3b
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-04-29 13:50:21 +0000

    Add questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,81 @@
+const questions = [
+    {
+        text: "What is the capital of Türkiye?",
+        answers: ["Istanbul", "Ankara", "Antalya"],
+        correct: "Ankara"
+    },
+    {
+        text: "What is the capital of Argentina?",
+        answers: ["Montevideo", "Santiago", "Buenos Aires"],
+        correct: "Buenos Aires"
+    },
+    {
+        text: "What is the capital of Ecuador?",
+        answers: ["Quito", "Lima", "Santiago"],
+        correct: "Quito"
+    },
+    {
+        text: "What is the capital of Armenia?",
+        answers: ["Batumi", "Tbilisi", "Yerevan"],
+        correct: "Yerevan"
+    },
+    {
+        text: "What is the capital of Nepal?",
+        answers: ["Kathmandu", "Phnom Penh", "Kolkata"],
+        correct: "Kathmandu"
+    },
+    {
+        text: "What is the capital of Croatia?",
+        answers: ["Bratislava", "Zagreb", "Belgrade"],
+        correct: "Zagreb"
+    },
+    {
+        text: "What is the capital of Nigeria?",
+        answers: ["Lagos", "Kinshasa", "Abuja"],
+        correct: "Abuja"
+    },
+    {
+        text: "What is the capital of Vietnam?",
+        answers: ["Phnom Penh", "Kathmandu", "Hanoi"],
+        correct: "Hanoi"
+    },
+    {
+        text: "What is the capital of Norway?",
+        answers: ["Oslo", "Stockholm", "Helsinki"],
+        correct: "Oslo"
+    },
+    {
+        text: "What is the capital of Uruguay?",
+        answers: ["Santiago", "Buenos Aires", "Montevideo"],
+        correct: "Montevideo"
+    },
+    {
+        text: "What is the capital of Peru?",
+        answers: ["Quito", "Lima", "Machu Picchu"],
+        correct: "Lima"
+    },
+    {
+        text: "What is the capital of Paraguay?",
+        answers: ["Montevideo", "Asuncion", "Santiago"],
+        correct: "Asuncion"
+    },
+    {
+        text: "What is the capital of Moldova?",
+        answers: ["Chisinau", "Sofia", "Bucharest"],
+        correct: "Chisinau"
+    },
+    {
+        text: "What is the capital of Spain?",
+        answers: ["Barcelona", "Valencia", "Madrid"],
+        correct: "Madrid"
+    },
+    {
+        text: "What is the capital of Ukraine?",
+        answers: ["Lviv", "Kyiv", "Odesa"],
+        correct: "Kyiv"
+    }
+];
+
 let username = document.getElementById("user");
 let questionsQuiz = document.getElementById("question-container");
 questionsQuiz.style.display = "none";
